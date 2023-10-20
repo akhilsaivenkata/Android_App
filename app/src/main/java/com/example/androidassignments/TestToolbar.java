4f23777e7de9643836ffc26e2989b02e23d5d6f1
akhilsaivenkata <akhilammu1@gmail.com>
2023-10-20 13:56:07 -0400

    Finished assignment2Part4

--- app/src/main/java/com/example/androidassignments/TestToolbar.java
+++ app/src/main/java/com/example/androidassignments/TestToolbar.java
@@ -9,10 +9,12 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.core.view.WindowCompat;
@@ -27,6 +29,7 @@ public class TestToolbar extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityTestToolbarBinding binding;
+    private String newMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,23 +59,25 @@ public class TestToolbar extends AppCompatActivity {
         // Inflate the menu from the XML resource file
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.toolbar_menu, menu);
+        newMessage = getString(R.string.Menu1);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         int id = item.getItemId();
         // Use a switch statement to determine which item was selected
 
         if (id == R.id.action_oneSai) {
             Log.d("Toolbar", "Choice 1 selected");
-            Snackbar.make(findViewById(R.id.action_oneSai), "You selected item 1", Snackbar.LENGTH_SHORT).show();
+            Snackbar.make(findViewById(R.id.action_oneSai), newMessage, Snackbar.LENGTH_SHORT).show();
             // Handle Choice 1
             return true;
         } else if (id == R.id.action_twoSai) {
             // Handle Choice 2
             Log.d("Toolbar", "Choice 2 selected");
-            Snackbar.make(findViewById(R.id.action_twoSai), "You selected item 2", Snackbar.LENGTH_SHORT).show();
+            Snackbar.make(findViewById(R.id.action_twoSai), R.string.Menu2, Snackbar.LENGTH_SHORT).show();
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setTitle(R.string.ToolDialog);
             builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
@@ -95,7 +100,8 @@ public class TestToolbar extends AppCompatActivity {
         } else if (id == R.id.action_threeSai) {
             // Handle Choice 3
             Log.d("Toolbar", "Choice 3 selected");
-            Snackbar.make(findViewById(R.id.action_threeSai), "You selected item 3", Snackbar.LENGTH_SHORT).show();
+            Snackbar.make(findViewById(R.id.action_threeSai), R.string.Menu3, Snackbar.LENGTH_SHORT).show();
+            showCustomDialog();
             return true;
         } else if (id == R.id.about_itemSai) {
             // toast msg details
@@ -106,6 +112,36 @@ public class TestToolbar extends AppCompatActivity {
         } else {
             return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void showCustomDialog() {
+        // Create a custom dialog with your layout
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogLayout = inflater.inflate(R.layout.custom_dialog_layout, null);
+
+        // Find the EditText view within the custom dialog layout
+        EditText newMessageEditText = dialogLayout.findViewById(R.id.newMessageEditText);
+
+        // Create an AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(dialogLayout);
+
+        // Set custom dialog actions
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button in the custom dialog
+                newMessage = newMessageEditText.getText().toString();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the custom dialog, do nothing
+            }
+        });
+
+        // Create and show the custom dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
     /*@Override
